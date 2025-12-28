277d868e435774e0d8a6c8cfc9982a3815bf78e6
Furtive_Pygmy <shayandp75@gmail.com>
2025-12-28 13:02:08 +0500

    moves source code to src

--- metro_system/Test.java
+++
@@ -1,9 +0,0 @@
-
-public class Test {
-public static void main(String[] args) {
-		
-		System.out.println("hello there");
-			System.out.println("i think its time we should do our first commit");
-
-	}
-}
